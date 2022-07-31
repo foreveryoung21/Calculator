4bb0d78bd1f2464ddf074fe22277a6f6a9e79960
Rodgers Kaunda <rodgerskaunda@Rodgerss-MacBook-Pro.local>
2022-07-31 17:27:35 +0100

    css and html complete for calcultor

---
+++ index.js
@@ -0,0 +1,47 @@
+// save value in variable sum.
+// 5+5*10
+// when you select an operation 
+// save different operations + * / 
+// when you press plus it keeps staving 
+
+// each operation has section of operation 
+// if the value is an operation then evaluate result and print
+// follow the following steps 
+// onchange event 
+
+// click event has the current number 
+// when you click an operator current number combine
+// when you pres the = operating on both the previous and new number 
+
+
+let plus = document.querySelector(".plus");
+let equal = document.querySelector(".equal");
+let input = document.querySelector("input");
+
+
+/*
+console.log(input);
+let total; 
+
+let previous =0;
+let current =0;
+
+function op(previous,current){
+    return previous+current;
+}
+
+plus.addEventListener("click",()=>{
+   
+    previous = parseInt(input.value);
+})
+
+equal.addEventListener("click",()=>{
+    let current = parseInt(input.value);
+    console.log(current+previous);
+
+})
+*/
+
+//hash table array linked list queue 
+
+// background-image: linear-gradient(to right top, #7f6bd1, #8170d5, #8475da, #867ade, #897fe2, #8780e5, #8680e9, #8481ec, #7c7ef0, #747bf3, #6a78f7, #5f75fb);    
