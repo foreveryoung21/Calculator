73f3a7942ee7132389610bfa39a1ac02e78adae3
Rodgers Kaunda <rodgerskaunda@Rodgerss-MacBook-Pro.local>
2022-08-01 21:02:45 +0100

    calcultor completed

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
-// save value in variable sum.
-// 5+5*10
-// when you select an operation 
-// save different operations + * / 
-// when you press plus it keeps staving 
-
-// each operation has section of operation 
-// if the value is an operation then evaluate result and print
-// follow the following steps 
-// onchange event 
-
-// click event has the current number 
-// when you click an operator current number combine
-// when you pres the = operating on both the previous and new number 
-
-
-
-/*
-console.log(input);
-let total; 
-
-let previous =0;
-let current =0;
-
-function op(previous,current){
-    return previous+current;
-}
-
-plus.addEventListener("click",()=>{
-   
-    previous = parseInt(input.value);
-})
-
-equal.addEventListener("click",()=>{
-    let current = parseInt(input.value);
-    console.log(current+previous);
-
-})
-*/
-
-//hash table array linked list queue 
-
 let buttons = document.querySelectorAll(".button");
 let inputDisplay = document.querySelector(".input-display");
 let calculatorBody = document.querySelector(".calculator-body");
+let calulationDisplay = document.querySelector(".calculation-display");
+console.log(calulationDisplay.innerText);
 
 function createTextNode(text){
     let textNode = document.createTextNode(text);
@@ -89,6 +49,7 @@ function operation(prev,op){
             }
             let elementText = event.target.innerText;
             inputDisplay.appendChild(createTextNode(elementText));
+            calulationDisplay.appendChild(createTextNode(elementText))
      
             
         }else if(event.target.matches(".button-clear")){
@@ -97,6 +58,7 @@ function operation(prev,op){
         }else if(event.target.matches(".button-operation")){
             let operationText = event.target.innerText;
             let values = inputDisplay.innerText;
+            calulationDisplay.appendChild(createTextNode(operationText));
             op = operation(values,operationText);
             clearText();
         }else if(event.target.matches(".button-equals")){
